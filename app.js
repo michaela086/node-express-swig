d57f81ec7b79697feb110cb455eba734608198d8
michaela086 <mike.miller@covenanteyes.com>
2014-11-12 14:31:13 -0500

    add in passport, and change the routing structure

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-var app = require('express')(),
-    swig = require('swig'),
-    people;
+var app = require('express')();
+var swig = require('swig');
+var people;
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session')
 var serverport = 3000;
 
+swig.setDefaults({ cache: false });
+
 app.engine('html', swig.renderFile);
+
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.set('view cache', false);
-swig.setDefaults({ cache: false });
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: "some secret key",
+    saveUninitialized: true, // (default: true)
+    resave: true, // (default: true)
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-app.use('/', routes);
-app.use('/users', users);
+eval(fs.readFileSync(__dirname + '/passport.js')+'');
+eval(fs.readFileSync(__dirname + '/routes.js')+'');
 
 app.listen(serverport);
 console.log('Application Started on http://localhost:'+serverport+'/');
---
+++ routes.js
@@ -0,0 +1,27 @@
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Foundation' });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user, message: req.flash('error') });
+});
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login');
+}
