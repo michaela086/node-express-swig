5ab95d101b5ed605780bb6f4b6036b244fbb8b05
michaela086 <mike.miller@covenanteyes.com>
2014-11-21 18:14:16 -0500

    update

--- server_config.js
+++
@@ -1,4 +0,0 @@
-module.exports.serverip = 'localhost';
-module.exports.serverport = '3000';
-module.exports.GOOGLE_CLIENT_ID = '777419445622-ki49vgoti9vq63q1mh5lro99lk6cs0i2.apps.googleusercontent.com';
-module.exports.GOOGLE_CLIENT_SECRET = 'WUtpYK08v-7_9ecPXSElN_7c';
